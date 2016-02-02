fa8d519911138502d54dad51d34bf1dd2fa28e66
Yu Dongfeng <yudongfeng@sjtu.edu.cn>
2016-02-02 21:52:27 +0800

    1

--- Modular Integer.hpp
+++ Modular Integer.hpp
@@ -1,53 +1,53 @@
 #include<bits/stdc++.h>
 using namespace std;
-template<class T,T P,int D=0>struct ModularInteger{
+template<class T,T P>struct ModularInteger{
     ModularInteger(T t=0):v(t){
         if(v<0||v>=P)
             v=(v%P+P)%P;
     }
-    ModularInteger<T,P,D>&operator=(T a){
+    ModularInteger<T,P>&operator=(T a){
         v=a;
         if(v<0||v>=P)
             v%=P;
         return*this;
     }
-    ModularInteger<T,P,D>operator-(){
+    ModularInteger<T,P>operator-(){
         return v?P-v:0;
     }
-    ModularInteger<T,P,D>&operator+=(ModularInteger<T,P,D>a){
+    ModularInteger<T,P>&operator+=(ModularInteger<T,P>a){
         return*this=*this+a;
     }
-    ModularInteger<T,P,D>&operator-=(ModularInteger<T,P,D>a){
+    ModularInteger<T,P>&operator-=(ModularInteger<T,P>a){
         return*this=*this-a;
     }
-    ModularInteger<T,P,D>&operator*=(ModularInteger<T,P,D>a){
+    ModularInteger<T,P>&operator*=(ModularInteger<T,P>a){
         return*this=*this*a;
     }
-    ModularInteger<T,P,D>&operator/=(ModularInteger<T,P,D>a){
+    ModularInteger<T,P>&operator/=(ModularInteger<T,P>a){
         return*this=*this/a;
     }
     T v;
 };
-template<class T,T P,int D>ModularInteger<T,P,D>pow(ModularInteger<T,P,D>a,long long b){
-    ModularInteger<T,P,D>r(1);
+template<class T,T P>ModularInteger<T,P>pow(ModularInteger<T,P>a,long long b){
+    ModularInteger<T,P>r(1);
     for(;b;b>>=1,a=a*a)
         if(b&1)
             r=r*a;
     return r;
 }
-template<class T,T P,int D>ModularInteger<T,P,D>inv(ModularInteger<T,P,D>a){
+template<class T,T P>ModularInteger<T,P>inv(ModularInteger<T,P>a){
     return pow(a,P-2);
 }
-template<class T,T P,int D>vector<ModularInteger<T,P,D> >sqrt(ModularInteger<T,P,D>a){
-    vector<ModularInteger<T,P,D> >r;
+template<class T,T P>vector<ModularInteger<T,P> >sqrt(ModularInteger<T,P>a){
+    vector<ModularInteger<T,P> >r;
     if(!a.v)
-        r.push_back(ModularInteger<T,P,D>(0));
+        r.push_back(ModularInteger<T,P>(0));
     else if(pow(a,P-1>>1).v==1){
         int s=P-1,t=0;
-        ModularInteger<T,P,D>b=1;
+        ModularInteger<T,P>b=1;
         for(;pow(b,P-1>>1).v!=P-1;b=rand()*1.0/RAND_MAX*(P-1));
         for(;s%2==0;++t,s/=2);
-        ModularInteger<T,P,D>x=pow(a,(s+1)/2),e=pow(a,s);
+        ModularInteger<T,P>x=pow(a,(s+1)/2),e=pow(a,s);
         for(int i=1;i<t;++i,e=x*x/a)
             if(pow(e,1<<t-i-1).v!=1)
                 x=x*pow(b,(1<<i-1)*s);
@@ -56,38 +56,38 @@ template<class T,T P,int D>vector<ModularInteger<T,P,D> >sqrt(ModularInteger<T,P
     }
     return r;
 }
-template<class T,T P,int D>ModularInteger<T,P,D>operator+(ModularInteger<T,P,D>a,ModularInteger<T,P,D>b){
-    ModularInteger<T,P,D>c(a.v+b.v);
+template<class T,T P>ModularInteger<T,P>operator+(ModularInteger<T,P>a,ModularInteger<T,P>b){
+    ModularInteger<T,P>c(a.v+b.v);
     if(c.v>=P)
         c.v-=P;
     return c;
 }
-template<class T,T P,int D>ModularInteger<T,P,D>operator-(ModularInteger<T,P,D>a,ModularInteger<T,P,D>b){
-    ModularInteger<T,P,D>c(a.v-b.v);
+template<class T,T P>ModularInteger<T,P>operator-(ModularInteger<T,P>a,ModularInteger<T,P>b){
+    ModularInteger<T,P>c(a.v-b.v);
     if(c.v<0)
         c.v+=P;
     return c;
 }
-template<class T,T P,int D>ModularInteger<T,P,D>operator*(ModularInteger<T,P,D>a,ModularInteger<T,P,D>b){
-    if(D)
-        return ModularInteger<T,P,D>((a.v*b.v-(long long )(((long double)a.v*b.v+0.5)/P)*P+P)%P);
+template<class T,T P>ModularInteger<T,P>operator*(ModularInteger<T,P>a,ModularInteger<T,P>b){
+    if(typeid(T)!=typeid(int))
+        return ModularInteger<T,P>((a.v*b.v-(long long )(((long double)a.v*b.v+0.5)/P)*P+P)%P);
     else
-        return ModularInteger<T,P,D>((long long)a.v*b.v%P);
+        return ModularInteger<T,P>((long long)a.v*b.v%P);
 }
-template<class T,T P,int D>ModularInteger<T,P,D>operator/(ModularInteger<T,P,D>a,ModularInteger<T,P,D>b){
+template<class T,T P>ModularInteger<T,P>operator/(ModularInteger<T,P>a,ModularInteger<T,P>b){
     return a*inv(b);
 }
-template<class T,T P,int D>bool operator==(ModularInteger<T,P,D>a,ModularInteger<T,P,D>b){
+template<class T,T P>bool operator==(ModularInteger<T,P>a,ModularInteger<T,P>b){
     return a.v==b.v;
 }
-template<class T,T P,int D>bool operator!=(ModularInteger<T,P,D>a,ModularInteger<T,P,D>b){
+template<class T,T P>bool operator!=(ModularInteger<T,P>a,ModularInteger<T,P>b){
     return a.v!=b.v;
 }
-template<class T,T P,int D>istream&operator>>(istream&s,ModularInteger<T,P,D>&a){
+template<class T,T P>istream&operator>>(istream&s,ModularInteger<T,P>&a){
     s>>a.v;
     return s;
 }
-template<class T,T P,int D>ostream&operator<<(ostream&s,ModularInteger<T,P,D>a){
+template<class T,T P>ostream&operator<<(ostream&s,ModularInteger<T,P>a){
     s<<a.v;
     if(a.v<0||a.v>=P)
         a.v%=P;
